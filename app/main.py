16b7071011949f4670c63e017f63ac1ab8d5fee4
00-hidan-00 <david.taranov@gmail.com>
2022-07-09 17:18:27 +0200

    edit Proof of concept 2.0

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
-from modeles.create_a_comment import CreateAComment
+from app.models import AddToLike
+from app.models.create_a_comment import CreateAComment
 
 
 def main_function():
     comment = CreateAComment.new_comment(user_id=31, comment='Good goods')
+    add_to_like = AddToLike(32, {"fwaf": "fwaf"})
+
+    print(add_to_like.user_id)
     print(comment)
